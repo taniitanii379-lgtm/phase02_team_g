f0909569449da1957c5aa43b9bee503cde078e98
yuri-kuramoto <s24b2032@bene.fit.ac.jp>
2025-10-19 15:06:34 +0900

    migration fileのかぶりを解消

--- database/seeders/ScoreSeeder.php
+++ database/seeders/ScoreSeeder.php
@@ -10,9 +10,9 @@ class ScoreSeeder extends Seeder
     public function run(): void
     {
         Score::insert([
-            ['user_id' => 1, 'score' => 80, 'created_at' => now(), 'updated_at' => now()],
-            ['user_id' => 1, 'score' => 90, 'created_at' => now(), 'updated_at' => now()],
-            ['user_id' => 1, 'score' => 70, 'created_at' => now(), 'updated_at' => now()],
+            ['quiz_id' => 1,'user_id' => 1, 'score' => 80, 'total_questions'=>3,'created_at' => now(), 'updated_at' => now()],
+            ['quiz_id' => 1,'user_id' => 2, 'score' => 90, 'total_questions'=>4,'created_at' => now(), 'updated_at' => now()],
+            ['quiz_id' => 1,'user_id' => 3, 'score' => 70, 'total_questions'=>5,'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
